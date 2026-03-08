2bae4cd46f04a77e2e518c0c38057d8e7c7b541c
gitlabbe <labj5746@usherbrooke.ca>
2026-03-07 20:05:34 -0500

    meme

--- frontend/src/EasterEgg.tsx
+++ frontend/src/EasterEgg.tsx
@@ -1,21 +1,28 @@
 import './App.css';
+import meme from'./image/meme.jpg'
 
 function EasterEgg() {
   return (
     <div className="meme">
-      <div>
-        Université de Sherbrooke
-      </div>
-        <div >
-            Équipe Artemus
-            <ul>
-                <li>
-                    Béatrice Gagnon
-                </li>
-                <li>
-                    Jean-François Labbé
-                </li>
-            </ul>
+        <div className="memeTextContainer">
+            <div className="memeText">
+                Université de Sherbrooke
+            </div>
+            <div className="memeText">
+                    Équipe Artemus
+                    <ul>
+                        <li>
+                            Béatrice Gagnon
+                        </li>
+                        <li>
+                            Jean-François Labbé
+                        </li>
+                    </ul>
+            </div>
+            </div>
+            
+        <div className='memeImage'>
+            <img src={meme} alt="meme drole"></img>
         </div>
     </div>
   );
